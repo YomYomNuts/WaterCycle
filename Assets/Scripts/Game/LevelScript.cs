f1673ed978e64a2ea8743bfb31ff432e0ae15cc3
Guillaume Noisette <noisette.guillaume@gmail.com>
2016-03-26 11:46:05 +0100

    Add level mouvement

---
+++ Assets/Scripts/Game/LevelScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+public class LevelScript : MonoBehaviour
+{
+    #region Public Attributes
+    public List<GameObject> PlanesLevels;
+    public float SpeedLevel;
+    public Vector3 EndLevelsPosition;
+    #endregion
+
+    #region Private Attributes
+    private Vector3 DiffBetweenLevels;
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        Assert.IsFalse(PlanesLevels.Count < 2);
+        DiffBetweenLevels = PlanesLevels[1].transform.position - PlanesLevels[0].transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Level Movement
+        Vector3 addPosition = new Vector3(0.0f, 0.0f, -SpeedLevel * Time.deltaTime);
+        foreach (GameObject level in PlanesLevels)
+            level.transform.position += addPosition;
+        for (int i = 0; i < PlanesLevels.Count; ++i)
+        {
+            GameObject level = PlanesLevels[i];
+            if (level.transform.position.z < EndLevelsPosition.z)
+            {
+                int previsouIndex = i - 1 < 0 ? PlanesLevels.Count - 1 : i - 1;
+                level.transform.position = PlanesLevels[previsouIndex].transform.position + DiffBetweenLevels;
+            }
+        }
+    }
+}
