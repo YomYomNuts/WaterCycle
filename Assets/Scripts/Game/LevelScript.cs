302d2b1ebbc8daa5781f30fffa6bae48b1036a74
Guillaume Noisette <noisette.guillaume@gmail.com>
2016-03-27 15:29:17 +0200

    Add Popup

--- Assets/Scripts/Game/LevelScript.cs
+++ Assets/Scripts/Game/LevelScript.cs
@@ -1,14 +1,13 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.Assertions;
 
 public class LevelScript : MonoBehaviour
 {
     #region Public Attributs
     public List<GameObject> PlanesLevels;
     public float SpeedLevel;
-    public Vector3 EndLevelsPosition;
+    [HideInInspector]
+    public bool DefilementStop = false;
     #endregion
 
     #region Private Attributs
@@ -17,24 +16,17 @@ public class LevelScript : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-        //DiffBetweenLevels = PlanesLevels[1].transform.position - PlanesLevels[0].transform.position;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        // Level Movement
-        Vector3 addPosition = new Vector3(0.0f, 0.0f, -SpeedLevel * Time.deltaTime);
-        foreach (GameObject level in PlanesLevels)
-            level.transform.position += addPosition;
-        /*for (int i = 0; i < PlanesLevels.Count; ++i)
+        if (!DefilementStop)
         {
-            GameObject level = PlanesLevels[i];
-            if (level.transform.position.z < EndLevelsPosition.z)
-            {
-                int previsouIndex = i - 1 < 0 ? PlanesLevels.Count - 1 : i - 1;
-                level.transform.position = PlanesLevels[previsouIndex].transform.position + DiffBetweenLevels;
-            }
-        }*/
+            // Level Movement
+            Vector3 addPosition = new Vector3(0.0f, 0.0f, -SpeedLevel * Time.deltaTime);
+            foreach (GameObject level in PlanesLevels)
+                level.transform.position += addPosition;
+        }
     }
 }
---
+++ Assets/Scripts/Game/PopUpScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpScript : MonoBehaviour
+{
+    #region Public Class
+    [System.Serializable]
+    public class SpritePopUp
+    {
+        public Sprite SpriteToSee;
+        public Sprite SpriteToPopUp;
+    }
+    #endregion
+
+    #region Public Attributs
+    public Image ImagePopUp;
+    public List<SpritePopUp> SpritesPopUp;
+    #endregion
+
+    #region Private Attributs
+    private static List<Sprite> SpritesDisplay;
+    private LevelScript _LevelScript;
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        ImagePopUp.enabled = false;
+        SpritesDisplay = new List<Sprite>();
+        _LevelScript = GetComponentInParent<LevelScript>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+
+    IEnumerator WaitingDescrutionObject(GameObject parGameObject)
+    {
+        while(parGameObject != null)
+        {
+            yield return 0.0f;
+        }
+        _LevelScript.DefilementStop = false;
+        ImagePopUp.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider parCollider)
+    {
+        SpriteRenderer spr = parCollider.gameObject.GetComponent<SpriteRenderer>();
+        if (spr != null && parCollider.gameObject.layer != Const.LAYER_LEVEL)
+        {
+            if (!SpritesDisplay.Contains(spr.sprite))
+            {
+                SpritePopUp spu = SpritesPopUp.Where(s => s.SpriteToSee == spr.sprite).First<SpritePopUp>();
+                ImagePopUp.sprite = spu.SpriteToPopUp;
+                ImagePopUp.enabled = true;
+                SpritesDisplay.Add(spr.sprite);
+                _LevelScript.DefilementStop = true;
+                StartCoroutine(WaitingDescrutionObject(parCollider.gameObject));
+            }
+        }
+    }
+}
